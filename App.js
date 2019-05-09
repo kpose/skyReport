50b8eb543acec827a808aaef69f14ada279f2add
kpose <ganihujude@gmail.com>
2019-05-09 10:39:52 +0100

    Start project and hardcode text attributes

---
+++ App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, TextInput, } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
+        <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
+        <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+
+        <TextInput
+          autoCorrect={false}
+          placeholder="Search For Your City"
+          placeholderTextColor="white"
+          style={styles.textInput}
+          clearButtonMode="always"
+        />
+
+      </KeyboardAvoidingView>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center',
+  },
+
+  largeText: {
+     fontSize: 44,
+     },
+  smallText: {
+     fontSize: 18,
+     },    
+});
